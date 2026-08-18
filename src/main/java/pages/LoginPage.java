1fc4cfc9c5085405e5c7747c33bca1e607bb49b6
Darren <darrenzheng1997@gmail.com>
2026-08-18 01:06:05 -0700

    working on login page tests, testing for error messages and active element on error

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,17 +7,21 @@ public class LoginPage extends BasePage {
     public static final By USERNAME = By.id("uname");
     public static final By PASSWORD = By.id("pass");
     public static final By EMAIL = By.id("passaddon");
-    public static final By LANGUAGE = By.id("selLanguage");
     public static final By LOGIN_BUTTON = By.cssSelector(".btn.btn-success");
-
+    public static final By PASSWORD_VISIBILITY_BUTTON = By.id("password-icon");
+    public static final By ERROR_MESSAGE = By.cssSelector(".h6.alert");
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
     public void login(String userName, String password, String email) {
-        this.type(USERNAME, userName);
+        this.fillLoginForm(userName, password, email);
+        this.click(LOGIN_BUTTON);
+    }
+
+    public void fillLoginForm(String username, String password, String email) {
+        this.type(USERNAME, username);
         this.type(PASSWORD, password);
         this.type(EMAIL, email);
-        this.click(LOGIN_BUTTON);
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.LoginPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class LoginTests {
 
@@ -30,6 +35,9 @@ public class LoginTests {
         driver.get(BASE_URL);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("", "", "");
+        WebElement activeElement = (WebElement) ((JavascriptExecutor) driver) .executeScript("return document.activeElement");
+        assertEquals(driver.findElement(LoginPage.USERNAME), activeElement);
+        assert !driver.findElement(LoginPage.ERROR_MESSAGE).isDisplayed();
     }
 
     @Test
@@ -37,6 +45,9 @@ public class LoginTests {
         driver.get(BASE_URL);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(USERNAME, PASSWORD, "invalid-email");
+        WebElement activeElement = (WebElement) ((JavascriptExecutor) driver) .executeScript("return document.activeElement");
+        assertEquals(driver.findElement(LoginPage.EMAIL), activeElement);
+        assert !driver.findElement(LoginPage.ERROR_MESSAGE).isDisplayed();
     }
 
     @Test
@@ -44,6 +55,16 @@ public class LoginTests {
         driver.get(BASE_URL);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login("", PASSWORD, EMAIL);
+        WebElement activeElement = (WebElement) ((JavascriptExecutor) driver) .executeScript("return document.activeElement");
+        assertEquals(driver.findElement(LoginPage.USERNAME), activeElement);
+        assert !driver.findElement(LoginPage.ERROR_MESSAGE).isDisplayed();
+    }
+
+    @Test
+    public void testInvalidUsername() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("Darren3", PASSWORD, EMAIL);
     }
 
     @Test
@@ -54,15 +75,69 @@ public class LoginTests {
     }
 
     @Test
+    public void testInvalidPassword() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(USERNAME, "invalid-password", EMAIL);
+    }
+
+    // to do
+    @Test
+    public void testPasswordVisibilityToggle() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.fillLoginForm(USERNAME, "test123", EMAIL);
+        driver.findElement(LoginPage.PASSWORD_VISIBILITY_BUTTON).click();
+    }
+
+    @Test
     public void testEmptyEmail() {
         driver.get(BASE_URL);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(USERNAME, PASSWORD, "");
+        assert loginPage.getText(LoginPage.ERROR_MESSAGE).contains("Something went wrong. Please try again.");
     }
 
     @Test
+    public void testNoDomainEmail() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        String emailWithoutDomain = "test@";
+        loginPage.login(USERNAME, PASSWORD, emailWithoutDomain);
+        String validationMessage = driver.findElement(LoginPage.EMAIL).getAttribute("validationMessage");
+        assert ("Please enter a part following '@'. '" + emailWithoutDomain + "' is incomplete.")
+                .equals(validationMessage);
+    }
 
+    @Test
+    public void testNoLocalPartEmail() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        String emailWithoutLocalPart = "@test.com";
+        loginPage.login(USERNAME, PASSWORD, emailWithoutLocalPart);
+        String validationMessage = driver.findElement(LoginPage.EMAIL).getAttribute("validationMessage");
+        assert ("Please enter a part followed by '@'. '" + emailWithoutLocalPart + "' is incomplete.")
+                .equals(validationMessage);
 
+    }
+
+    @Test
+    public void testNoDomainSuffixEmail() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        String emailWithoutDomainSuffix = "test@test";
+        loginPage.login(USERNAME, PASSWORD, emailWithoutDomainSuffix);
+        assert loginPage.getText(LoginPage.ERROR_MESSAGE).contains("Something went wrong. Please try again.");
+    }
+
+    @Test
+    public void testFieldsAfterRefresh() {
+        driver.get(BASE_URL);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.fillLoginForm(USERNAME, PASSWORD, EMAIL);
+        driver.navigate().refresh();
+
+    }
 
     @AfterEach
     void tearDown() {
